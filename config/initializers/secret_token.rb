cfe0009defe74798ff50bbf30e50921f561e6708
Mayra Cabrera <mcabrera@dalai.com>
2014-06-03 12:36:35 -0500

    Creates an user scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Directory::Application.config.secret_token = 'e1d5c526b46b0153a65e7a74c5bca42bf4bf60532813942fd80887fe12cb4a4babd4ea47a3879611bc57dd9329787713939b281c65ef47ad0aa75a1b68510d9e'
